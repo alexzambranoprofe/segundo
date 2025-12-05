962343eff94e6096a26ef15192e0d9991d0bcc54
azambri <azambri@misena.edu.co>
2025-12-04 21:00:23 -0500

    cambie el logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import adsoLogo from "./assets/adso.svg";
 import "./App.css";
 
 function App() {
@@ -8,14 +8,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://serviciosfinancierosena.blogspot.com/" target="_blank">
+          <img src={adsoLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Hola Chicos ADSO feliz Navidad</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          El contador esta en {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
